210f03025ba5a88a1f6f4e94b328b7e42cfc5bfe
yuli setyawan <yulisetyawan2008@gmail.com>
2020-07-02 20:03:45 +0800

    pertama commit

---
+++ database/migrations/2020_07_02_115719_create_jawaban_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('isi');
+            $table->integer('penanya_id');
+            $table->integer('is_selected');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawaban');
+    }
+}
